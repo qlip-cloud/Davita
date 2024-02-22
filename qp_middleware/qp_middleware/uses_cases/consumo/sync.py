f48417db13e49422b4ee6d6c59dfd2cb0d421f47
Rafael Licett <rafael.licett@mentum.group>
2024-02-22 19:31:10 +0000

    enabled sync button consumoUpload by error_sync and error_sync_connection

--- qp_middleware/qp_middleware/uses_cases/consumo/sync.py
+++ qp_middleware/qp_middleware/uses_cases/consumo/sync.py
@@ -23,7 +23,7 @@ def handler(upload_id):
                             'is_background': True, 
                             'start_date': now() 
                         })
-    
+
     frappe.enqueue(
                 sync,
                 #queue='long',                
@@ -42,7 +42,7 @@ def handler(upload_id):
 def sync(upload_id):
 
     try:                      
-        consumos = frappe.get_list("qp_md_Consumo", filters = {"upload_id": upload_id, "is_valid": True, "is_error_sync": ["!=", True]}, fields = ["name", "request", "dimension_code", 'nombre'])
+        consumos = frappe.get_list("qp_md_Consumo", filters = {"upload_id": upload_id, "is_valid": True, "is_sync": ["!=", True]}, fields = ["name", "request", "dimension_code", 'nombre'])
 
         send_consumos(consumos)
 
@@ -65,16 +65,19 @@ def set_consumoUploadStadistic(upload_id, error = False):
     
     result = frappe.db.sql(sql,as_dict = True)
 
-    frappe.db.set_value('qp_md_ConsumoUpload', upload_id, 
+    if not error:
+
+        error = result[0].get("count_error") > 0
+    
+        frappe.db.set_value('qp_md_ConsumoUpload', upload_id, 
                         {
                             'is_background': False,
                             'send_success': result[0].get("count_sync"),
                             'send_error': result[0].get("count_error"),
                             "is_error_sync": error,
-                            "is_error_connection": True if result[0].get("count_error_connection") > 0 else False,
+                            "is_error_connection": result[0].get("count_error_connection") > 0,
                             "end_date": now()
                          })
-
 
 def send_consumos(consumos):
 
@@ -135,8 +138,6 @@ def get_urls():
 
     return enviroment.get_url_ws_protocol(enpoint.url)
 
-
-
 def send_document(payloads, url):
 
     #payloads = list(map(lambda consumo: consumo.get("request"), consumos))
@@ -146,9 +147,7 @@ def send_document(payloads, url):
     payload_xml = """<soap:Envelope xmlns:nav="urn:microsoft-dynamics-schemas/codeunit/registroDiarioProducto" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"> <soap:Body> <nav:registroDiarioProducto> <nav:diario>{}</nav:diario> </nav:registroDiarioProducto> </soap:Body> </soap:Envelope>""".format(payloads)
     
     payload_xml = payload_xml.replace("'","")
-    
-    #URL_HEADER = "https://api.businesscentral.dynamics.com/v2.0/a1af66a5-d7b4-43a1-9663-3f02fecf8060/MIDDLEWARE/api/v2.0/companies(798ec2fe-ddfe-ed11-8f6e-6045bd3980fd)/salesInvoices"
-    
+        
     add_header = {
         "SOAPAction": "#POST"
     }
